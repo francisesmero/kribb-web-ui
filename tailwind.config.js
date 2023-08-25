b2090c341903f8ff9c8a4e9c036179bebe4dd45e
Francis Esmero <francis.esmero@gmail.com>
2023-08-25 19:08:48 +0800

    feature: add nav, herobanner, searchbar

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    'src/components/**/*.{html,js}',
+    'src/pages/**/*.{html,js}',
+    'src/index.html',
+  ],theme: {
+    extend: {
+      colors: {
+        "light-secondary": "#314862",
+        "light-accent": "#12263a",        
+      }
+    }
+
+  }
+  // ...
+}
